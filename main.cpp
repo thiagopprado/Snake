736de7ab9d9859e960494309d8b0b935fe89db1a
Thiago Pereira do Prado <thiagopereiraprado@gmail.com>
2018-04-04 00:48:02 -0300

    Comentario

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main (){
     CONSOLE_CURSOR_INFO cursor = {1, FALSE};
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor); // Desliga o Cursor
     Snake game(50,20);
     game.Inicia();
     return 0;
